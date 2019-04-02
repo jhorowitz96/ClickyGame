f8781b46f6ba29d6b58cf7fc4690517a23f906ec
Greeley Wentz <jasonhorowitz@Jasons-MacBook-Pro.local>
2019-04-02 19:54:18 -0400

    Updates

--- clicky/src/components/Header/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./style.css";
-
-function Header(props) {
-  return <div className="header" status={props.status} style={{backgroundColor: "lightgray"}}>{props.children}</div>;;
-}
-
-export default Header;
---
+++ precache-manifest.22c1b6e0e0acb3d270aed4cb4b8fa888.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "712384c81c4228d8ed37",
+    "url": "/static/js/main.bfebf40b.chunk.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "712384c81c4228d8ed37",
+    "url": "/static/css/main.ada4a5f2.chunk.css"
+  },
+  {
+    "revision": "b85177958be08cad37e837a457444cab",
+    "url": "/index.html"
+  }
+];
