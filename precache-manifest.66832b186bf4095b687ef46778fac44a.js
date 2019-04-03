3b668f437349c1ae9092dc663c05353fa627405e
Greeley Wentz <jasonhorowitz@Jasons-MacBook-Pro.local>
2019-04-02 20:24:06 -0400

    Updates

---
+++ precache-manifest.66832b186bf4095b687ef46778fac44a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "95fffdb9eb36913c2817",
+    "url": "/ClickyGame/static/js/runtime~main.1eb9bb0a.js"
+  },
+  {
+    "revision": "712384c81c4228d8ed37",
+    "url": "/ClickyGame/static/js/main.bfebf40b.chunk.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/ClickyGame/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "712384c81c4228d8ed37",
+    "url": "/ClickyGame/static/css/main.ada4a5f2.chunk.css"
+  },
+  {
+    "revision": "9edf401606b782262fec7cc7685878f0",
+    "url": "/ClickyGame/index.html"
+  }
+];
